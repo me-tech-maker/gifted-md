2f508e7b703f865411e7cf9b48a9b9c1160cd240
Maurice Gift <founder@giftedtech.co.ke>
2025-10-07 09:14:57 +0300

    GIFTED MD

--- gift/index.js
+++ gift/index.js
@@ -1,6 +1,7 @@
 const { evt, gmd, commands } = require('./gmdCmds');
 const config = require('../config');
 
+const { UpdateDB, setCommitHash, getCommitHash } = require('./autoUpdate');
 const { createContext, createContext2 } = require('./gmdHelpers');
 const { getSudoNumbers, setSudo, delSudo } = require('./gmdSudoUtil');
 const { getMediaBuffer, getFileContentType, bufferToStream, uploadToGiftedCdn, uploadToGithubCdn, uploadToPixhost, uploadToImgBB, uploadToPasteboard, uploadToCatbox } = require('./gmdFunctions3');
@@ -8,4 +9,4 @@ const { logger, emojis, GiftedAutoReact, GiftedTechApi, GiftedApiKey, GiftedAnti
 const { toAudio, toVideo, toPtt, formatVideo, formatAudio, monospace, runtime, sleep, gmdFancy, GiftedUploader, stickerToImage, formatBytes, gmdBuffer, webp2mp4File, gmdJson, latestWaVersion, gmdRandom, isUrl, gmdStore, isNumber, loadSession, verifyJidState } = require('./gmdFunctions');
 
 
-module.exports = { evt, gmd, config, emojis, commands, toAudio, toVideo, toPtt, formatVideo, uploadToGiftedCdn, uploadToGithubCdn, formatAudio, runtime, sleep, gmdFancy, GiftedUploader, stickerToImage, monospace, formatBytes, createContext, createContext2, getSudoNumbers, setSudo, delSudo, GiftedTechApi, GiftedApiKey, getMediaBuffer, getFileContentType, bufferToStream, uploadToPixhost, uploadToImgBB, uploadToPasteboard, uploadToCatbox, GiftedAutoReact, GiftedChatBot, GiftedAntiLink, GiftedAntiDelete, GiftedAnticall, GiftedPresence, GiftedAutoBio, logger, gmdBuffer, webp2mp4File, gmdJson, latestWaVersion, gmdRandom, isUrl, gmdStore, isNumber, loadSession, verifyJidState };
+module.exports = { evt, gmd, config, emojis, commands, toAudio, toVideo, toPtt, formatVideo, uploadToGiftedCdn, uploadToGithubCdn, UpdateDB, setCommitHash, getCommitHash, formatAudio, runtime, sleep, gmdFancy, GiftedUploader, stickerToImage, monospace, formatBytes, createContext, createContext2, getSudoNumbers, setSudo, delSudo, GiftedTechApi, GiftedApiKey, getMediaBuffer, getFileContentType, bufferToStream, uploadToPixhost, uploadToImgBB, uploadToPasteboard, uploadToCatbox, GiftedAutoReact, GiftedChatBot, GiftedAntiLink, GiftedAntiDelete, GiftedAnticall, GiftedPresence, GiftedAutoBio, logger, gmdBuffer, webp2mp4File, gmdJson, latestWaVersion, gmdRandom, isUrl, gmdStore, isNumber, loadSession, verifyJidState };
